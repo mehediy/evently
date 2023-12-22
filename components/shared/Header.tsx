11d009b73a10d843ad471fc9ecedd9425135a783
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-12-23 01:17:04 +0600

    added clerk auth

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,25 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link className="w-36" href={"/"}><Image src={"/assets/images/logo.svg"} width={128} height={38} alt="Logo"/></Link>
+        <div className="w-32 flex gap-3 justify-end">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/"/>
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size={'lg'} >
+              <Link href={"/sign-in"}>Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
   )
 }
 
