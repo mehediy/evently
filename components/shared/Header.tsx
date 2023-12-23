1fb7a4039039c8ca3764facf1c80a6b0f1d0c3db
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-12-23 15:06:08 +0600

    added nav items

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,26 +1,41 @@
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from "../ui/button"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
-        <Link className="w-36" href={"/"}><Image src={"/assets/images/logo.svg"} width={128} height={38} alt="Logo"/></Link>
-        <div className="w-32 flex gap-3 justify-end">
+        <Link className="w-36" href={"/"}>
+          <Image
+            src={"/assets/images/logo.svg"}
+            width={128}
+            height={38}
+            alt="Logo"
+          />
+        </Link>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+        <div className="w-32 flex gap-3 items-center justify-end">
           <SignedIn>
-            <UserButton afterSignOutUrl="/"/>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
-            <Button asChild className="rounded-full" size={'lg'} >
+            <Button asChild className="rounded-full" size={"lg"}>
               <Link href={"/sign-in"}>Login</Link>
             </Button>
           </SignedOut>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavItems = () => {
+  const pathname = usePathname();
+  return (
+    <ul className="flex md:flex-between flex-col w-full items-start gap-5 md:flex-row">
+      {headerLinks.map((item) => {
+        const isActive = pathname == item.route;
+        return (
+          <li
+            key={item.route}
+            className={`${
+              isActive && "text-primary-500"
+            } flex-center p-medium-16 whitespace-nowrap`}
+          >
+            <Link href={item.route}>{item.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default NavItems;
