0cd71e169ad41a417ff5e8546981c6803fe6f741
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-12-23 15:15:30 +0600

    Update Footer.tsx

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href={"/"}>
+          <Image
+            src={"/assets/images/logo.svg"}
+            height={38}
+            width={128}
+            alt="Logo"
+          />
+        </Link>
+        <p>All rights reserved</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
