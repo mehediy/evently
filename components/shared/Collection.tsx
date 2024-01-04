91a1583aaf5228e8bfad110f8fab3078886b8c15
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2024-01-04 23:12:30 +0600

    related events

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -7,7 +7,7 @@ type CollectionProps = {
   emptyStateSubtext: string;
   limit: number;
   urlParamName?: string;
-  page: number;
+  page: number | string;
   totalPages?: number;
   collectionType?: "Events_Organized" | "My_Tickets" | "All_Events";
 };
